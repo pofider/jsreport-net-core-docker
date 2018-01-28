e6dee99d9d699d9f855575088e87ef570f0dac09
pofider <jan.blaha@hotmail.com>
2018-01-28 16:53:32 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using jsreport.Local;
+using jsreport.Types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockerNetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rs = new LocalReporting()
+               .UseBinary(RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
+                   jsreport.Binary.JsReportBinary.GetBinary() :
+                   jsreport.Binary.Linux.JsReportBinary.GetBinary())
+               .AsUtility()
+               .Create();
+
+            var report = rs.RenderAsync(new RenderRequest()
+            {
+                Template = new Template()
+                {
+                    Recipe = Recipe.PhantomPdf,
+                    Engine = Engine.None,
+                    Content = "Hello"
+                }
+            }).Result;
+
+            var content = new StreamReader(report.Content).ReadToEnd();
+
+            Console.WriteLine(content);
+        }
+    }
+}
